8b1f4526b31bb902d3cd50e1e8620f4fb57f6fb2
silvistershebin <silvistershebin@gmail.com>
2020-06-11 16:54:05 +0530

    Bubble Sort

---
+++ EASY/Bubble Sort/Solution1.cpp
@@ -0,0 +1,27 @@
+/****************************************************
+Basic Bubble Sort with O(n) for best case
+TC: O(n)
+SC: O(1)
+*****************************************************/
+
+#include <vector>
+using namespace std;
+
+vector<int> bubbleSort(vector<int> array) {
+  bool isSorted = false;
+	if(array.empty())
+		return {};
+	int j = 0;
+	while(!isSorted) {
+		isSorted = true;
+		for(int i = 0; i < array.size() - j - 1; i++) {
+			if(array[i] > array[i+1]) {
+				int temp = array[i];
+				swap(array[i], array[i+1]);
+				isSorted = false;
+			}
+		}
+		j++;
+	}
+	return array;
+}
